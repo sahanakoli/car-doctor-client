05a873e8f22706531829f58cc0ea3ee273761d43
Sahana Koli <sahanasultanakoli@gmail.com>
2023-10-25 00:02:57 +0600

    navbar and footer added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Shared/Navbar";
+import Footer from "../Shared/Footer";
 
 const Main = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Navbar></Navbar>
+            <Footer></Footer>
         </div>
     );
 };
