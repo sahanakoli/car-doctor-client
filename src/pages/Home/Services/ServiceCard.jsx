aee5c8f4c5592e1b9adfe0b1bc6a8b8cf4fe07a4
Sahana Koli <sahanasultanakoli@gmail.com>
2023-10-25 18:39:03 +0600

    service section added

---
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/prop-types */
+
+
+const ServiceCard = ({ service }) => {
+    const { title, img, price } = service;
+    return (
+        <div className="mt-8">
+            <div className="card w-96 h-5/6 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src={img} alt="" className="rounded-xl" />
+                </figure>
+                <div className="card-body ">
+                    <h2 className="card-title">{title}</h2>
+                    <p className="text-xl text-orange-500">Price: ${price}</p>
+                    <div className="card-actions justify-end">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path d="M4.5 12H19.5M19.5 12L12.75 5.25M19.5 12L12.75 18.75" stroke="#FF3811" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceCard;
